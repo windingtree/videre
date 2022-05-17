01c2f21e5302934e118a963bc2d722295ea18e33
mfw78 <mfw78@protonmail.com>
2022-05-17 15:22:59 +0000

    feat: 🎸 eip712 generics protobuf
    A utility library implemented in generics that can take a protobuf
    message and either sign it, or verify it.

--- packages/sdk/src/utils/signing.ts
+++ packages/sdk/src/utils/signing.ts
@@ -1,3 +1,63 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import { Wallet, utils } from "ethers";
+import { TypedDataDomain, TypedDataField } from "@ethersproject/abstract-signer";
+
+const log = console.log;
+
+export interface SignedMessage {
+  signature: Uint8Array
+}
+
+/**
+ * Generics utility function for EIP712 signing of protobuf messages.
+ * @param domain for EIP712 signature
+ * @param types used within the data structure
+ * @param msg being signed
+ * @param signer of the signature
+ * @returns a protobuf message with the signature placed as per callback
+ */
+ export async function createSignedMessage<T extends SignedMessage>(
+  domain: TypedDataDomain,
+  types: Record<string, TypedDataField[]>,
+  msg: T,
+  signer: Wallet
+): Promise<T> {
+  try {
+    const values: Omit<T, "signature"> = msg
+    const signature = await signer._signTypedData(domain, types, values);
+    return {
+      ...msg,
+      signature: utils.arrayify(signature)
+    }
+  } catch(e) {
+    throw e
+  }
+}
+
+/**
+ * Generics utility function for verifying EIP712 signing of protobuf messages.
+ * @param which service provider to check on chain
+ * @param domain for EIP712 signature verification
+ * @param types used within the data structure
+ * @param msg being verified
+ * @param verifier callback function to assert truthfullness
+ * @returns true if message is verified, false otherwise
+ */
+ export async function verifyMessage<T extends SignedMessage, U>(
+  which: U, 
+  domain: TypedDataDomain,
+  types: Record<string, TypedDataField[]>,
+  msg: T,
+  verifier: (which: U, who: string) => Promise<boolean>
+): Promise<boolean> {
+  try {
+    // get the values excluding the signature
+    const values: Omit<T, "signature"> = msg
+    // get the signing key
+    const who = utils.verifyTypedData(domain, types, values, msg.signature)
+    // now pass the signer and value to a function to verify
+    return verifier(which, who)
+  } catch(e) {
+    log(e)
+    return false
+  }
 }
