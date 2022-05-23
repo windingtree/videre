64d36c4d922562d7d8f4d1139d4579d6e2c7c320
mfw78 <mfw78@protonmail.com>
2022-05-23 14:04:17 +0000

    feat: 🎸 stub chain state
    Add in a stub type for exporting, as well as EIP-712 for hashing. The
    stubs aren't signed, but a hash is determined on-chain and stored.

---
+++ packages/sdk/src/chainstate/stub.ts
@@ -0,0 +1,10 @@
+import { BidTerm } from "../proto/bidask";
+import { ERC20Native } from "../proto/token";
+
+export type StubState = {
+  which: Uint8Array,
+  params: Uint8Array,
+  items: Uint8Array[],
+  terms: BidTerm[],
+  cost: ERC20Native
+}
