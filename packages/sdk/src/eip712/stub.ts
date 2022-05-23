e298740562b808d9f957853d5d22007645fed20b
mfw78 <mfw78@protonmail.com>
2022-05-23 14:04:17 +0000

    feat: 🎸 stub chain state
    Add in a stub type for exporting, as well as EIP-712 for hashing. The
    stubs aren't signed, but a hash is determined on-chain and stored.

---
+++ packages/sdk/src/eip712/stub.ts
@@ -0,0 +1,15 @@
+import { TypedDataField } from "@ethersproject/abstract-signer";
+import { ERC20Native } from "./token"
+import { BidTerm } from "./bidask";
+
+export const StubState: Record<string, TypedDataField[]> = {
+  StubState: [
+    { name: "which", type: "bytes32" },
+    { name: "params", type: "bytes32" },
+    { name: "items", type: "bytes32[]" },
+    { name: "terms", type: "BidTerm[]" },
+    { name: "cost", type: "ERC20Native" }
+  ]
+}
+StubState["BidTerm"] = BidTerm.BidTerm
+StubState["ERC20Native"] = ERC20Native.ERC20Native
