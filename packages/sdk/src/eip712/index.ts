a1a1766b48470bfa20dc364e96afe894b8e6aa98
mfw78 <mfw78@protonmail.com>
2022-05-23 10:37:10 +0000

    refactor: 💡 add role for validateSigner

--- packages/sdk/src/eip712/index.ts
+++ packages/sdk/src/eip712/index.ts
@@ -13,10 +13,24 @@ export {
   token
 }
 
-export async function validateProvider(
+/**
+ * Check if a signatory is valid for a service provider
+ * @param which service provider's signer is being validated
+ * @param role that the signer should possess
+ * @param who is claiming to be an authorized signer
+ * @param registry holding all valid service providers to check
+ * @returns true if a valid signer, false otherwise
+ */
+export async function validateSigner(
   which: Uint8Array,
+  role: number,
   who: string,
   registry: ServiceProviderRegistry
 ): Promise<boolean> {
-  return await registry.can(which, 3, who);
+  const roles = constants.AccessRoles
+  if (role != roles.API_ROLE || role != roles.BIDDER_ROLE) {
+    return new Promise((resolve) => resolve(false) )
+  } else {
+    return await registry.can(which, constants.AccessRoles.BIDDER_ROLE, who);
+  }
 }
