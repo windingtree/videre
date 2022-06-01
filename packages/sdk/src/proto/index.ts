820decfe766f6970adb4511610c73527c098285d
mfw78 <mfw78@protonmail.com>
2022-06-01 08:47:54 +0000

    fix: 🐛 missing stub protobuf
    Adds in the stub protobuf, predominately for use in key-value storage
    databases for clients to maintain state.

--- packages/sdk/src/proto/index.ts
+++ packages/sdk/src/proto/index.ts
@@ -1,12 +1,16 @@
 import * as bidask from "./bidask"
+import * as date from "./date"
 import * as pingpong from "./pingpong"
 import * as storage from "./storage"
+import * as stub from "./stub"
 import * as timestamp from "./timestamp"
 import * as token from "./token"
 export {
   bidask,
+  date,
   pingpong,
   storage,
+  stub,
   timestamp,
   token
 }
